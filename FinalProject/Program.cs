29ffb75613af63e70625d4bd35bee14f5c0335b3
Hergusbergus <zane.ammerman@gmail.com>
2025-06-02 16:36:34 -0600

    Final Project Init
    Simplest project. Will expand with software design patterns.

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please identify yourself.");
+string userInput = Console.ReadLine().ToLower();
+if (userInput == "earth")
+{
+    Console.WriteLine("Hello, World!");
+}
+else
+{
+    Console.WriteLine("Buzz off!");
+}
