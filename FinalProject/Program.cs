bc45ff80723c150f5e36db8a5492d992d2947cb8
Hergusbergus <zane.ammerman@gmail.com>
2025-06-02 23:54:00 -0600

    Observer Pattern
    Notifies observers of the identity to alter state accordingly

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,14 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
 
-// Abstract product
-public abstract class Identifier
+// Observer interface
+public interface IIdentificationObserver
 {
+    void OnIdentification(string planet, string response);
+}
+
+// Concrete observers
+public class LoggingObserver : IIdentificationObserver
+{
+    public void OnIdentification(string planet, string response)
+    {
+        Console.WriteLine($"[LOG] Identification attempt: {planet} -> {response}");
+    }
+}
+
+public class WelcomeObserver : IIdentificationObserver
+{
+    public void OnIdentification(string planet, string response)
+    {
+        if (planet.ToLower() == "earth" || planet.ToLower() == "mars")
+        {
+            Console.WriteLine("Welcome to our cosmic identification system!");
+        }
+    }
+}
+
+// Subject interface
+public interface IIdentificationSubject
+{
+    void Attach(IIdentificationObserver observer);
+    void Detach(IIdentificationObserver observer);
+    void NotifyObservers(string planet, string response);
+}
+
+// Modified Identifier to include subject behavior
+public abstract class Identifier : IIdentificationSubject
+{
+    private List<IIdentificationObserver> _observers = new List<IIdentificationObserver>();
+    protected string _planet;
+
+    public Identifier(string planet)
+    {
+        _planet = planet;
+    }
+
     public abstract string GetResponse();
+
+    public void Attach(IIdentificationObserver observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void Detach(IIdentificationObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void NotifyObservers(string planet, string response)
+    {
+        foreach (var observer in _observers)
+        {
+            observer.OnIdentification(planet, response);
+        }
+    }
+
+    public string ProcessIdentification()
+    {
+        string response = GetResponse();
+        NotifyObservers(_planet, response);
+        return response;
+    }
 }
 
 // Concrete products
 public class EarthIdentifier : Identifier
 {
+    public EarthIdentifier() : base("earth") { }
+
     public override string GetResponse()
     {
         return "Hello, World!";
@@ -17,6 +87,8 @@ public class EarthIdentifier : Identifier
 
 public class MarsIdentifier : Identifier
 {
+    public MarsIdentifier() : base("mars") { }
+
     public override string GetResponse()
     {
         return "Welcome, Martian!";
@@ -25,6 +97,8 @@ public class MarsIdentifier : Identifier
 
 public class UnknownIdentifier : Identifier
 {
+    public UnknownIdentifier() : base("unknown") { }
+
     public override string GetResponse()
     {
         return "Buzz off!";
@@ -55,6 +129,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create observers
+        var logger = new LoggingObserver();
+        var welcomer = new WelcomeObserver();
+
         Console.WriteLine("Please identify yourself.");
         string userInput = Console.ReadLine();
         
@@ -62,6 +140,11 @@ class Program
         IdentifierFactory factory = new PlanetIdentifierFactory();
         Identifier identifier = factory.CreateIdentifier(userInput);
         
-        Console.WriteLine(identifier.GetResponse());
+        // Attach observers
+        identifier.Attach(logger);
+        identifier.Attach(welcomer);
+        
+        // Process identification (this will notify observers)
+        Console.WriteLine(identifier.ProcessIdentification());
     }
 }
