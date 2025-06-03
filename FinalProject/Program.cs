9b1f151bb59351f61533fafc0a9e7c6529a99e4d
Hergusbergus <zane.ammerman@gmail.com>
2025-06-02 23:28:05 -0600

    Factory Method
    Added Factory Method in case I wanted to create more responses to certain identities.

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,10 +1,67 @@
-﻿Console.WriteLine("Please identify yourself.");
-string userInput = Console.ReadLine().ToLower();
-if (userInput == "earth")
+﻿using System;
+
+// Abstract product
+public abstract class Identifier
 {
-    Console.WriteLine("Hello, World!");
+    public abstract string GetResponse();
 }
-else
+
+// Concrete products
+public class EarthIdentifier : Identifier
 {
-    Console.WriteLine("Buzz off!");
+    public override string GetResponse()
+    {
+        return "Hello, World!";
+    }
+}
+
+public class MarsIdentifier : Identifier
+{
+    public override string GetResponse()
+    {
+        return "Welcome, Martian!";
+    }
+}
+
+public class UnknownIdentifier : Identifier
+{
+    public override string GetResponse()
+    {
+        return "Buzz off!";
+    }
+}
+
+// Creator (Factory)
+public abstract class IdentifierFactory
+{
+    public abstract Identifier CreateIdentifier(string input);
+}
+
+// Concrete creator
+public class PlanetIdentifierFactory : IdentifierFactory
+{
+    public override Identifier CreateIdentifier(string input)
+    {
+        return input.ToLower() switch
+        {
+            "earth" => new EarthIdentifier(),
+            "mars" => new MarsIdentifier(),
+            _ => new UnknownIdentifier()
+        };
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please identify yourself.");
+        string userInput = Console.ReadLine();
+        
+        // Using the factory to create the appropriate identifier
+        IdentifierFactory factory = new PlanetIdentifierFactory();
+        Identifier identifier = factory.CreateIdentifier(userInput);
+        
+        Console.WriteLine(identifier.GetResponse());
+    }
 }
